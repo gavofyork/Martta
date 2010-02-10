821340a59afe4e1c71953602f72b3861d6c8a896
Gav <gavin@quidprocode.co.uk>
2010-02-10 18:43:19 +0100

    Killed bad files.

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the exscalibar package
-#
-0 4	* * *	root	[ -x /usr/bin/exscalibar_maintenance ] && /usr/bin/exscalibar_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(exscalibar):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="exscalibar" command="/usr/bin/exscalibar"
